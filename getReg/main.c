016ee6864712fa7d76008ceabb96e634f01acfcf
Brett Clavier <brettclavier@gmail.com>
2011-10-04 19:01:08 -0500

    Initial Commit

---
+++ getReg/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  getReg
+//
+//  Created by Brett Clavier on 10/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
